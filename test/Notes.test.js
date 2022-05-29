a7865f978ccab599bcbb19db14ac8d23e5dac199
Ed <ed321@hotmail.co.uk>
2022-05-29 17:46:46 +0100

    Refactored

--- test/Notes.test.js
+++ test/Notes.test.js
@@ -1,22 +1,22 @@
 const Notes = artifacts.require('Notes');
 
 contract('Notes', () => {
+  let notes;
+  beforeEach(async () => {
+    notes = await Notes.deployed();
+  })
   it('Deploys a smart contract', async () => {
-    const notes = await Notes.deployed();
     assert(notes.address !== '');
   })
   it('can run a function on the smart contract', async () => {
-    const notes = await Notes.deployed();
     const result = await notes.readFromContract();
     assert(result === 'Preset note');
   })
   it('the initial value of the owner of a smart contract', async () => {
-    const notes = await Notes.deployed();
     const result = await notes.getOwner();
     assert(result === 'Unknown User');
   })
   it('can set the value of the owner of this contract', async () => {
-    const notes = await Notes.deployed();
     await notes.setOwner('Ed');
     assert(await notes.getOwner() === 'Ed');
   })
