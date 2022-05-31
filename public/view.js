fbc5341f0b761ef3235fe1624b54d1362e91b3bb
Ed <ed321@hotmail.co.uk>
2022-05-31 21:38:00 +0100

    Form created for resetting the name on the contract

--- public/view.js
+++ public/view.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', async () => {
   const name = document.getElementById('owner');
+  const setName = document.getElementById('setOwner');
+  setName.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('pressed');
+  })
   const owner = await getOwner();
-  name.innerHTML =  owner.name;
+  name.innerHTML = owner.name;
 })
 
 const getOwner = async () => {
